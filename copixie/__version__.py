762ecc08727bc64c05e47ae8c92c036fa353c153
Samuel Prince <samuel.prince-drouin@umontreal.ca>
2024-10-01 10:56:18 -0400

    Added module docstring

--- copixie/__version__.py
+++ copixie/__version__.py
@@ -1,19 +1,17 @@
-"""
-Copyright (C) 2023-2024 Samuel Prince <samuel.prince-drouin@umontreal.ca>
-
-This file is a part of CoPixie.
-
-This file may be used under the terms of the GNU General Public License
-version 3 as published by the Free Software Foundation and appearing in
-the file LICENSE included in the packaging of this file.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
+# Copyright (C) 2023-2024 Samuel Prince <samuel.prince-drouin@umontreal.ca>
+# 
+# This file is a part of CoPixie.
+# 
+# This file may be used under the terms of the GNU General Public License
+# version 3 as published by the Free Software Foundation and appearing in
+#the file LICENSE included in the packaging of this file.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 __version__ = '0.1.0'
