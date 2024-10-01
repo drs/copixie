ab3d9553c9d111196fcb0148cd77599cd6cb1848
Samuel Prince <samuel.prince-drouin@umontreal.ca>
2024-10-01 04:23:12 -0400

    Changed module name from dctracker to copixie

--- setup.py
+++ setup.py
@@ -9,42 +9,29 @@ import os
 import sys
 from shutil import rmtree
 
-from setuptools import find_packages, setup, Command
+from setuptools import setup, Command
 
-# Package meta-data.
-NAME = 'dctracker'
+# package meta-data
+NAME = 'copixie'
 DESCRIPTION = 'A tool that identifies colocalization events between single-particle tracks in multiple channels.'
 URL = 'https://github.com/drs/copixie'
 EMAIL = 'samuel.prince-drouin@umontreal.ca'
 AUTHOR = 'Samuel Prince'
 REQUIRES_PYTHON = '>=3.7.0'
-
-# What packages are required for this module to be executed?
 REQUIRED = [
     'configobj', 'pandas', 'imageio'
 ]
 
-# What packages are optional?
-EXTRAS = {
-    # 'fancy feature': ['django'],
-}
-
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
-
 here = os.path.abspath(os.path.dirname(__file__))
 
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+# import the README and use it as the long-description.
 try:
     with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
         long_description = '\n' + f.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
 
-# Load the package's __version__.py module as a dictionary.
+# load the package's __version__.py module as a dictionary
 about = {}
 project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
 with open(os.path.join(here, project_slug, '__version__.py')) as f:
@@ -87,7 +74,6 @@ class UploadCommand(Command):
         sys.exit()
 
 
-# Where the magic happens:
 setup(
     name=NAME,
     version=about['__version__'],
@@ -98,20 +84,16 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    # packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    # If your package is a single module, use this instead of 'packages':
-    py_modules=['dctracker'],
+    py_modules=['copixie'],
 
     entry_points={
-        'console_scripts': ['copixie=dctracker.main:main'],
+        'console_scripts': ['copixie=copixie.main:main'],
     },
     install_requires=REQUIRED,
-    extras_require=EXTRAS,
     include_package_data=True,
     license='GPLv3',
+    # trove classifiers (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
     classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
